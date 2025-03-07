65b77b7afafff1dfd2840919cfde542f02be798f
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2025-03-07 04:03:50 -0800

    Delete Automatic-PR-Message-Generator-Sairammotupalli directory

--- Automatic-PR-Message-Generator-Sairammotupalli/deepseek-r1/test-set/test_file_2.py
+++
@@ -1,7 +0,0 @@
-# Input string
-s = "Geeks for Geeks"
-
-# Split the string into words, reverse the list of words, and join them back
-reversed_words = ' '.join(s.split()[::-1])
-
-print(reversed_words)
